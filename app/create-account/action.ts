7f30d7dbcff76c2228cc7ce087f61a43f48615f6
SeokJeong0411 <dk04188@naver.com>
2024-06-19 23:28:37 +0900

    240619

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,24 +1,77 @@
 "use server";
 
-import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, USERNAME_MAX_LENGTH, USERNAME_MIN_LENGTH } from "@/lib/constants";
+import {
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  USERNAME_MAX_LENGTH,
+  USERNAME_MIN_LENGTH,
+} from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
 
 const passwordRegex = PASSWORD_REGEX;
 
-const checkPasswords = ({ password, confirmPassword }: { password: string; confirmPassword: string }) =>
-  password === confirmPassword;
+// 비밀번호 및 비밀번호 확인이 동일한지 체크
+const checkPasswords = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => password === confirmPassword;
+
+// 유저명 중복 체크
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username: username,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return !Boolean(user);
+};
+
+// E-mail 중복 체크
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email: email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
 
 const formSchema = z
   .object({
-    username: z.string().min(USERNAME_MIN_LENGTH).max(USERNAME_MAX_LENGTH).toLowerCase().trim(),
-    email: z.string().email().toLowerCase(),
-    password: z
+    username: z
       .string()
-      .min(5)
-      .regex(passwordRegex, "A password must have lowercase, UPPERCASE, a number and special characters."),
+      .min(USERNAME_MIN_LENGTH)
+      .max(USERNAME_MAX_LENGTH)
+      .toLowerCase()
+      .trim()
+      .refine(checkUniqueUsername, "This Username is already taken"),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .refine(
+        checkUniqueEmail,
+        "This is account already registered with that E-mail"
+      ),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    // .regex(passwordRegex, "A password must have lowercase, UPPERCASE, a number and special characters."),
     confirmPassword: z.string().min(PASSWORD_MIN_LENGTH),
   })
-  .refine(checkPasswords, { message: "Both Passwords should be the Same!", path: ["confirmPassword"] });
+  .refine(checkPasswords, {
+    message: "Both Passwords should be the Same!",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -28,10 +81,15 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirmPassword: formData.get("confirmPassword"),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // check username valid
+    // check email valid
+    // hash password
+    // save user to db
+    // log in user
+    // redirect "/home"
   }
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const USERNAME_MIN_LENGTH = 4;
 export const USERNAME_MAX_LENGTH = 10;
-export const PASSWORD_MIN_LENGTH = 5;
+export const PASSWORD_MIN_LENGTH = 4;
 export const PASSWORD_REGEX = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/);
