abfea443860d9d6392fc43413097d9ecbaa729d6
SeokJeong0411 <dk04188@naver.com>
2024-06-24 21:46:10 +0900

    240624

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -8,6 +8,10 @@ import {
 } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const passwordRegex = PASSWORD_REGEX;
 
@@ -85,11 +89,27 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
-    // check username valid
-    // check email valid
-    // hash password
-    // save user to db
-    // log in user
-    // redirect "/home"
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const cookie = await getIronSession(cookies(), {
+      cookieName: "Orange-Karrot",
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    //@ts-ignore
+    cookie.id = user.id;
+    await cookie.save();
+
+    redirect("/profile");
   }
 }
