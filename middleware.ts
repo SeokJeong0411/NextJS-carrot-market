bef33780d7134ea2748f551a6d6028a134e8b3ae
SeokJeong0411 <dk04188@naver.com>
2024-07-04 20:31:38 +0900

    240704

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,32 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
   const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
+  const exist = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exist) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exist) {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
   }
 }
+
+// 실행 위치와 실행하지 않을 위치를 설정
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
